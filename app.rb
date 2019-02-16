0dbf2a567afb8dd86202f41a268a00b43cd7007a
Anton Buryka <anton.buryka@binomo.com>
2019-02-16 22:33:48 +0300

    change db name

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/activerecord'
 require 'net/ftp'
 
 configure :production do
-  set :database, {adapter: 'postgresql', encoding: 'unicode', database: 'telegram_bot_test_dev', pool: 2, host: ENV['FTP_HOST'], port: 5432, username: ENV['FTP_USER'], password: ENV['FTP_PASS']}
+  set :database, {adapter: 'postgresql', encoding: 'unicode', database: 'rng_prod', pool: 2, host: ENV['FTP_HOST'], port: 5432, username: ENV['FTP_USER'], password: ENV['FTP_PASS']}
 end
 
 class Book < ActiveRecord::Base
